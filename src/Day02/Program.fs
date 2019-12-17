75b169e1ab63f9fd64e76add3752fca54e4717f3
Anton Herzog <info@antonherzog.com>
2019-12-17 12:17:32 +0100

    Renaming and refactoring according to part two nomenclature

--- src/Day02/Program.fs
+++ src/Day02/Program.fs
@@ -4,16 +4,16 @@ open System
 
 let input = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,1,19,6,23,2,23,13,27,1,27,5,31,2,31,10,35,1,9,35,39,1,39,9,43,2,9,43,47,1,5,47,51,2,13,51,55,1,55,9,59,2,6,59,63,1,63,5,67,1,10,67,71,1,71,10,75,2,75,13,79,2,79,13,83,1,5,83,87,1,87,6,91,2,91,13,95,1,5,95,99,1,99,2,103,1,103,6,0,99,2,14,0,0"
 
-type Instructions = (int*int*int) 
+type Parameters = (int*int*int) 
 
-type Operation = 
-    | Add of Instructions
-    | Mul of Instructions
+type Instruction = 
+    | Add of Parameters
+    | Mul of Parameters
     | Exit
 
 type State = {
-    Program : Map<int, int>
-    NextIndex : int
+    Memory : Map<int, int>
+    InstructionPointer : int
 }
 
 
@@ -21,7 +21,7 @@ let log<'a> s a =
     printfn "%s %A" s a
     a
 
-let getInstructions {Program=p;NextIndex=i} =
+let getInstructions {Memory=p;InstructionPointer=i} =
     let ai = p.[i+1]
     let bi = p.[i+2]
     let a = p.[ai]
@@ -30,7 +30,7 @@ let getInstructions {Program=p;NextIndex=i} =
     (a, b, t)
 
 let parseOperation state =
-    let {Program=p;NextIndex=i} = state
+    let {Memory=p;InstructionPointer=i} = state
     match p.[i] with
     | 1     -> Add <| getInstructions state
     | 2     -> Mul <| getInstructions state
@@ -38,20 +38,20 @@ let parseOperation state =
     | _     -> raise (ArgumentException "foo")
 
 let applyAdd add  state =
-    let {Program=p} = state
+    let {Memory=p} = state
     let (a,b,t) = add
     let sum = a + b
     let updatedMap = Map.add t sum p
-    {state with Program = updatedMap};
+    {state with Memory = updatedMap};
 
 let applyMull add  state =
-    let {Program=p} = state
+    let {Memory=p} = state
     let (a,b,t) = add
     let sum = a * b
     let updatedMap = Map.add t sum p
-    {state with Program = updatedMap};
+    {state with Memory = updatedMap};
 
-let furtherIndex state = {state with NextIndex = state.NextIndex + 4}
+let furtherIndex state = {state with InstructionPointer = state.InstructionPointer + 4}
 
 let rec solveProgram state =
     match parseOperation state with
@@ -64,18 +64,24 @@ let parseProgram (input : string) =
     |> Array.mapi (fun i v -> (i,(int)v))
     |> Map.ofArray
 
-let createSolverInput program = {Program = program; NextIndex=0}
+let createSolverInput program = {Memory = program; InstructionPointer=0}
 
 let parseSolverInput = parseProgram >> createSolverInput
 
 let solve = parseSolverInput >> solveProgram
 
-let printableProgram {Program=p} =
+let printableProgram {Memory=p} =
     p |> Map.toArray |> Array.map snd |> Array.fold (fun s value-> sprintf "%s%i;"s value ) ""
 
 let solveForString s = printfn "Result for [%s] %s" s (s |> solve |> printableProgram)
 
-let partOneInput = input |> parseProgram|> Map.add 1 12 |> Map.add 2 2 |> createSolverInput
+let parametrizeProgram (noun, verb) =  Map.add 1 noun >> Map.add 2 verb
+
+let partOneParametrization = parametrizeProgram (12, 2)
+
+let partOneInput = input |> parseProgram |> partOneParametrization |> createSolverInput
+
+
 
 [<EntryPoint>]
 let main argv =
@@ -83,5 +89,5 @@ let main argv =
     solveForString "2,3,0,3,99"
     solveForString "2,4,4,5,99,0"
     solveForString "1,1,1,4,99,5,6,0,99"
-    printfn "Part One Result(should be 3790645): %i" (partOneInput |> solveProgram).Program.[0]
+    printfn "Part One Result(should be 3790645): %i" (partOneInput |> solveProgram).Memory.[0]
     0 // return an integer exit code
