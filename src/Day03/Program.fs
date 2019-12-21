7ed5b573c756339fff166414b8e8945e0489e43a
Anton Herzog <info@antonherzog.com>
2019-12-21 12:44:50 +0100

    Cleanup

--- src/Day03/Program.fs
+++ src/Day03/Program.fs
@@ -78,16 +78,16 @@ let layoutWires =
     >> List.head
     >> (fun (x, y) -> Math.Abs x + Math.Abs y)
 
-let example1 = @"R75,D30,R83,U83,L12,D49,R71,U7,L72
+let partOneExample1 = @"R75,D30,R83,U83,L12,D49,R71,U7,L72
 U62,R66,U55,R34,D71,R55,D58,R83";
 
-let example2 = @"R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+let partOneExample2 = @"R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
 U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
 
 [<EntryPoint>]
 let main _ =
     
-    printfn "%A" (layoutWires example1)
-    printfn "%A" (layoutWires example2)
-    printfn "%A" (layoutWires userInput)
+    printfn "%A\t(159  expected)" (layoutWires partOneExample1)
+    printfn "%A\t(135  expected)" (layoutWires partOneExample2)
+    printfn "%A\t(1225 expected)" (layoutWires userInput)
     0 // return an integer exit code
